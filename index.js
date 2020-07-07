2fcdc76739ba186de4571fd54f73fcd05333c6cd
Gustavo Lopes <lopes.gustavojf@gmail.com>
2020-07-06 22:08:04 -0300

    Isola busca de usuários

--- index.js
+++ index.js
@@ -7,7 +7,13 @@ const port = 3000;
 app.get('/user/:id', (req, res) => {
 	const id = req.params.id;
 	const user = getUserById(id);
-	return res.json(user);
+
+	if (user == null) {
+		return res.sendStatus(404)
+	} else {
+		return res.json(user);
+	}
+
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${ port }`));
--- services/user.js
+++ services/user.js
@@ -1,27 +1,27 @@
+const users = [
+	{
+		id: '1',
+		name: 'Gustavo Moraes Lopes',
+		birthday: '1994-04-20',
+		cpf: '111.111.111-11',
+		password: 'mysupersecretpassword',
+		email: 'meuemail@gmail.com',
+		course: 'engenharia',
+		rg: 'mg-111.111.11',
+		phone: '(11)91234-5678',
+		address: {
+			city: 'Juiz de Fora',
+			street: 'Rio Branco',
+			number: 10,
+			neighbourhood: 'Centro',
+		},
+	},
+];
+
 function getUserById(id) {
-	if (id === '1') {
-		return {
-			id: '1',
-			name: 'Gustavo Moraes Lopes',
-			birthday: '1994-04-20',
-			cpf: '111.111.111-11',
-			password: 'mysupersecretpassword',
-			email: 'meuemail@gmail.com',
-			course: 'engenharia',
-			rg: 'mg-111.111.11',
-			phone: '(11)91234-5678',
-			address: {
-				city: 'Juiz de Fora',
-				street: 'Rio Branco',
-				number: 10,
-				neighbourhood: 'Centro',
-			}
-		};
-	} else {
-		return null;
-	}
+	return users.find(user => user.id === id);
 }
 
 module.exports = {
-	getUserById
+	getUserById,
 };
