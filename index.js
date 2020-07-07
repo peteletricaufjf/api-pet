7bd0607e2205cd9663e570029f5d26b5a87a81d6
Gustavo Lopes <lopes.gustavojf@gmail.com>
2020-07-06 21:40:26 -0300

    Adiciona getUserById

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const { getUserById } = require('./services/user');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
-app.get('/user', (req, res) => res.json({
-	name: 'Gustavo',
-}));
-app.post('/', (req, res) => res.send('olá post'));
-app.get('*', (req, res) => res.send('nao achei'));
+app.get('/user/:id', (req, res) => {
+	const id = req.params.id;
+	const user = getUserById(id);
+	return res.json(user);
+});
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${ port }`));
