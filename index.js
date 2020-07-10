dcf24bc8c7af14bb39b2218cc0e8488b3354e76c
Gustavo Lopes <lopes.gustavojf@gmail.com>
2020-07-10 13:51:21 -0300

    Atualiza porta para ser dinâmica

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const { UserService } = require('./services/user');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
